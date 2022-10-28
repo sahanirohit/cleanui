233e8ad986199f06764a34e4cef6ed4ede6718a7
sahanirohit <rohitjobeis@gmail.com>
2022-10-28 07:59:27 +0530

    navbar created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#00020f",
+      accent: "#ccd3ff",
+      "secondary-accent": "#e0e5ff",
+    },
     extend: {},
   },
   plugins: [],
